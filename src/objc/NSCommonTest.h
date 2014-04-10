0768c477c6faf1cdd3515ae136b45b2c0f0bcc0c
Gal Dolber <gal.dolber@gmail.com>
2014-04-10 16:55:45 -0300

    Adding NSCommon test + add long double type

---
+++ src/objc/NSCommonTest.h
@@ -0,0 +1,15 @@
+//
+//  NSCommonTest.h
+//  mpos
+//
+//  Created by Gal Dolber on 4/10/14.
+//  Copyright (c) 2014 zuldi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSCommonTest : NSObject
+
++ (void) runtests;
+
+@end
